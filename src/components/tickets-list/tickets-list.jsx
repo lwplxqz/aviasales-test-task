720a3a563f49cc09c13506b5503d3a26f2b7fb30
lwplxqz <solovev.andrey7@gmail.com>
2022-12-25 22:01:30 +0300

    changes

--- src/components/tickets-list/tickets-list.jsx
+++ src/components/tickets-list/tickets-list.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 import './tickets-list.scss'
 import Ticket from '../ticket/ticket'
+import API from '../../services/aviasales-service';
 
 function TicketsList() {
+    const service = new API
+
+    const getTT = async ()  => {
+        const gaysheesh = await service.getTickets()
+        console.log(gaysheesh);
+    }
+    
+    console.log(getTT())
+
     return (
         <ul className='main__tickets-list tickets-list'>
             <Ticket />
--- src/services/aviasales-service.js
+++ src/services/aviasales-service.js
@@ -1,6 +1,16 @@
 /* eslint-disable class-methods-use-this */
-// export default class AviaAPI {
-//     getTickets = async() => {
-//       const response = await fetch('')
-//     }
-// }
+export default class API {
+	API_BASE = 'https://aviasales-test-api.kata.academy';
+
+	getTickets = async () => {
+		const idResponse = await fetch(`${this.API_BASE}/search`);
+		const { searchId } = await idResponse.json();
+
+		const response = await fetch(
+			`${this.API_BASE}/tickets?searchId=${searchId}`
+		);
+		const { tickets } = await response.json();
+		console.log(tickets);
+		return tickets;
+	};
+}
